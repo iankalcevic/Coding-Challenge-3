2384c1e2e2fb88cf237c50b9a8b654760f7a4e56
iankalcevic <iankalcevic@usf.edu>
2025-02-02 13:11:28 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -4,3 +4,9 @@ prices.push(50); // Add a new price to array
 prices.shift(); // Remove the first price from array
 console.log("Updated price list:". prices); // Log updated price list
 
+// Task 2 - Modifying customer orders
+let orders = [10,20,30,40,50] // Declare an array of orders
+orders[2] +=5; // Increase the third order by five
+let totalOrders = orders.reduce((total, orders) => total + orders, 0); // Calculate the total of orders
+console.log("Updated orders:", orders) // Updated array
+console.log("Total order count:", totalOrders); // Total order count
