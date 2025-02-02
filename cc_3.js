40dd472a648012fae22384d8fe466829382ee029
iankalcevic <iankalcevic@usf.edu>
2025-02-02 13:06:12 -0500

    Task 1 - Product Price Management

--- cc_3.js
+++ cc_3.js
@@ -1,1 +1,6 @@
+//Task 1 - Product price management scenario
+let prices = [5,10,20,30,40]: // Declare an array of prices
+prices.push(50); // Add a new price to array
+prices.shift(); // Remove the first price from array
+console.log("Updated price list:". prices); // Log updated price list
 
